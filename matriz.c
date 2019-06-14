0cb0ba899d51e69fab2704c4199203e9891fe0a9
willdc4 <williamdavicoelho@gmail.com>
2019-06-14 18:44:29 -0300

    acabou

--- matriz.c
+++ matriz.c
@@ -1,10 +1,13 @@
+// o exercicio cria uma matriz a partir de um arquivo txt no formato do "matriz.txt"
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <mpi.h>
 #include <math.h>
 
+int auxiliar;
 int main(int argc, char **argv){
+	// cria a matriz a partir do arquivo
 	FILE *file = fopen ("matriz.txt", "r" );
 	char* m1s;
 	char* aux;
@@ -32,29 +35,53 @@ int main(int argc, char **argv){
 			}
 	}
 	int proc, nprocs, **num;
+	int** m3 = new int[lin1][col2];
 	float subtotal, total;
+
+
+
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 	MPI_Comm_rank(MPI_COMM_WORLD, &proc);
-	
-	int m3[lin1][col2];
-	int r = lin1/nprocs;
-	for(int i = proc*r; i <(proc+1)*r; ++i){
-		for(int j = 0; j < col1; ++j){
-			for(int m = 0;m < col2; ++m){
-				int val = 0;
-				for(int n = 0; n < lin2; ++n){
-					val += m1[i][j] * m2[n][m];
+	// calcula a razao
+	int r = ceil(lin1/nprocs);
+	// o ultimo processo fica com o trabalho de realizar operações sobressalentes em caso da razao não ser um numero exato
+	if(proc == nprocs-1){
+		if(lin1 % nprocs != 0){
+			int tam = lin1 - (r*nprocs);
+			for(int i = proc*r; i <((proc+1)*r)+tam; ++i){
+				printf("i =   %i",i);
+				for(int j = 0; j < col2; ++j){
+					int val = 0;
+					for(int m = 0; m < col1; ++m){
+						val += m1[i][m] * m2[m][j];
+					}
+					m3[i][j] = val;
 				}
-				m3[i][m] = val;
+			}
+				
+		}
+		else{
+			for(int i = proc*r; i <(proc+1)*r; ++i){
+				for(int j = 0; j < col2; ++j){
+					int val = 0;
+					for(int m = 0; m < col1; ++m){
+						val += m1[i][m] * m2[m][j];
+					}
+					m3[i][j] = val;
+				}
 			}
 		}
-	}
-	for(int i = 0; i < lin1; ++i){
-		for(int j = 0; j < col2; ++j){
-			printf("%i   ",m3[i][j]);
+	}else{
+		for(int i = proc*r; i <(proc+1)*r; ++i){
+			for(int j = 0; j < col2; ++j){
+				int val = 0;
+				for(int m = 0; m < col1; ++m){
+					val += m1[i][m] * m2[m][j];
+				}
+			m3[i][j] = val;
+			}
 		}
-		printf("\n");
 	} 
     fclose ( file );
 
