40e1daa44dfbadf962e753e5651a572b823e0526
willdc4 <williamdavicoelho@gmail.com>
2019-06-14 18:06:23 -0300

    exercicio2.c somatorio adicionado

--- exercicio2.c
+++ exercicio2.c
@@ -4,10 +4,10 @@
 
 int main(int argc, char **argv){
     int proc, total, subtotal;
-    
+    int size;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &proc);
-    
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
     subtotal = proc + 1;
     
     MPI_Reduce(&subtotal, &total, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
@@ -15,9 +15,10 @@ int main(int argc, char **argv){
     if (proc == 0) {
         printf("total = %d, rank = %d\n", total, proc);
     }
-    
     MPI_Finalize();
-    
+    if (proc == 0) {
+        printf("valor calculado: %d\n", (size * (size + 1)) / 2);
+    }
     return 0;
 }
 
